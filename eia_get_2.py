3d80d6b8be8d51801d4d87734719663839e11521
adamheisey22 <admrussel@gmail.com>
2023-12-01 09:10:21 -0700

    final?

--- eia_get_2.py
+++ eia_get_2.py
@@ -21,10 +21,10 @@ def convert_to_dataframe(api_data):
     try:
         series_data = api_data['response']['data']
         df = pd.DataFrame(series_data)
-
+        
         # Convert 'subba' column to string
         df['subba'] = df['subba'].astype(str)
-
+        
         df['period'] = pd.to_datetime(df['period'])
         return df
     except KeyError as e:
@@ -42,30 +42,28 @@ def save_to_parquet(df, file_path):
     fp.write(file_path, combined_df, compression='SNAPPY', row_group_offsets=500000)
 
 if __name__ == "__main__":
-    api_url = "https://api.eia.gov/v2/electricity/rto/region-sub-ba-data/data/?api_key=WOLU5avflX0tFNVkVuShl4z2Pj3vyee7Z5n40O46&frequency=hourly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc"
-
-    target_date = datetime(2015, 1, 1, 0)  # Change this to your target date
+    api_url = "https://api.eia.gov/v2/electricity/rto/region-sub-ba-data/data/?frequency=hourly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc"
     chunk_size = 5000
-    current_date = datetime.now()  # Start from the current date
+    current_offset = 0
 
-    while current_date >= target_date:
-        print(f"Fetching data for {current_date} to {current_date - timedelta(hours=chunk_size)}")
-
-        api_data = fetch_eia_data(api_url, offset=0, length=chunk_size)
-
+    while True:
+        print(f"Fetching data for offset {current_offset} to {current_offset + chunk_size}")
+        
+        api_data = fetch_eia_data(api_url, offset=current_offset, length=chunk_size)
+        
         if api_data is not None:
             df = convert_to_dataframe(api_data)
-
-            if df is not None:
+            
+            if df is not None and not df.empty:
                 print(df.head())
-
+                
                 # Save data to Parquet file (append mode)
                 save_to_parquet(df, "output_data.parquet")
-
-                # Update current date for the next iteration
-                current_date -= timedelta(hours=chunk_size)
+                
+                # Update offset for the next iteration
+                current_offset += chunk_size
             else:
-                print("Failed to convert data to DataFrame.")
+                print("No more data available.")
                 break
         else:
             print("Failed to fetch data.")
