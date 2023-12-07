f97bd5b7052724f07baa6fc966cc6af393f7a739
adamheisey22 <admrussel@gmail.com>
2023-12-07 14:10:17 -0700

    up

--- imputation demand.py
+++ imputation demand.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
-def impute_demand(df, forecast_table, demand_range_table, exception_list):
+def impute_demand(df, forecast_column, demand_range_table, exception_list):
     for ba in df['BA'].unique():
         ba_mask = df['BA'] == ba
 
-        forecast_values = forecast_table.get(ba, {})
-        demand_range = demand_range_table.get(ba, {'min': 0, 'max': float('inf')})
-
         # Rule 1: Use demand forecast value if valid and not in the exception list
-        forecast_mask = ~df['Hour'].isin(forecast_values.keys()) | (df['BA'].isin(exception_list))
-        df.loc[ba_mask & forecast_mask & df['Demand'].isna(), 'Demand'] = df['Hour'].map(forecast_values)
+        forecast_mask = ~pd.isna(df[forecast_column]) | df['BA'].isin(exception_list)
+        df.loc[ba_mask & forecast_mask & df['Demand'].isna(), 'Demand'] = df[forecast_column]
 
         # Rule 2: Use prior hour's demand value if valid
         df.loc[ba_mask & df['Demand'].isna(), 'Demand'] = df['Hour'].map(df[ba_mask & (df['Hour'] == df['Hour'] - 1)]['Demand'])
@@ -21,10 +18,10 @@ def impute_demand(df, forecast_table, demand_range_table, exception_list):
         df.loc[ba_mask & df['Demand'].isna(), 'Demand'] = 0
 
         # Check if demand data is reported but out of reasonable range
-        range_mask = (df['Demand'] < demand_range['min']) | (df['Demand'] > demand_range['max'])
+        range_mask = (df['Demand'] < demand_range_table[ba]['min']) | (df['Demand'] > demand_range_table[ba]['max'])
 
         # Rule 1: Use demand forecast value if valid and not in the exception list
-        df.loc[ba_mask & range_mask & forecast_mask, 'Demand'] = df['Hour'].map(forecast_values)
+        df.loc[ba_mask & range_mask & forecast_mask, 'Demand'] = df[forecast_column]
 
         # Rule 2: Use prior hour's demand value if valid
         df.loc[ba_mask & range_mask, 'Demand'] = df['Hour'].map(df[ba_mask & (df['Hour'] == df['Hour'] - 1)]['Demand'])
@@ -38,4 +35,5 @@ def impute_demand(df, forecast_table, demand_range_table, exception_list):
     return df
 
 # Example usage:
-# df = impute_demand(df, forecast_table, demand_range_table, exception_list)
+# df = impute_demand(df, 'Forecast', demand_range_table, exception_list)
+
