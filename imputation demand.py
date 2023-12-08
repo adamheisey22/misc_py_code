dcf9fd50785dc823a67941f548f117ff8a537ce3
adamheisey22 <admrussel@gmail.com>
2023-12-08 07:56:07 -0700

    iterrows

--- imputation demand.py
+++ imputation demand.py
@@ -1,50 +1,69 @@
 import pandas as pd
 
-def impute_demand(df, forecast_table, demand_range_table):
+def impute_demand(df):
+    forecast_exception_list = set(['BA1', 'BA2', 'BA3'])  # Replace with your actual exception list
+    valid_demand_range = {
+        'BA1': (100, 500),
+        'BA2': (200, 600),
+        'BA3': (150, 450)
+        # Add more BA entries as needed
+    }  # Replace with your actual demand range lookup
+
     for index, row in df.iterrows():
-        ba = row['BA']  # Replace 'BA' with the actual column name for BA
-        hour = row['Hour']  # Replace 'Hour' with the actual column name for Hour
-        demand = row['Demand']  # Replace 'Demand' with the actual column name for Demand
+        ba = row['BA']
+        demand = row['demand']
+        hour = row['hour']
+        date = row['date']
+        forecast_demand = row['forecast_demand']
 
-        # Check if demand data is missing
-        if pd.isna(demand):
-            # Rule 1: Use demand forecast value if valid and not in the exception list
-            forecast_value = forecast_table.get(ba, {}).get(hour)
-            if forecast_value is not None and ba not in exception_list:
-                df.at[index, 'Demand'] = forecast_value
+        if pd.isnull(demand):
+            imputed_demand = None
+
+            # Rule 1: Use demand forecast value if available and not in the exception list
+            if ba not in forecast_exception_list and pd.notnull(forecast_demand):
+                imputed_demand = forecast_demand
+
+            # Rule 2: Use prior hour's demand value if available
+            elif hour > 0:
+                prior_hour_demand = df.at[index - 1, 'demand']
+                if pd.notnull(prior_hour_demand):
+                    imputed_demand = prior_hour_demand
+
+            # Rule 3: Use prior day's demand value for the corresponding hour if available
+            elif date > df['date'].min():
+                prior_day_demand = df[(df['BA'] == ba) & (df['date'] == date - pd.DateOffset(days=1)) & (df['hour'] == hour)]['demand'].values
+                if len(prior_day_demand) > 0 and pd.notnull(prior_day_demand[0]):
+                    imputed_demand = prior_day_demand[0]
+
+            if imputed_demand is not None:
+                df.at[index, 'demand'] = imputed_demand
             else:
-                # Rule 2: Use prior hour's demand value if valid
-                prior_hour_demand = df[(df['BA'] == ba) & (df['Hour'] == hour - 1)]['Demand'].values
-                if len(prior_hour_demand) > 0 and not pd.isna(prior_hour_demand[0]):
-                    df.at[index, 'Demand'] = prior_hour_demand[0]
-                else:
-                    # Rule 3: Use prior day's demand value for the corresponding hour if valid
-                    prior_day_demand = df[(df['BA'] == ba) & (df['Hour'] == hour - 24)]['Demand'].values
-                    if len(prior_day_demand) > 0 and not pd.isna(prior_day_demand[0]):
-                        df.at[index, 'Demand'] = prior_day_demand[0]
+                df.at[index, 'demand'] = 0  # Rule 4: No imputation is performed
 
-        # Check if demand data is reported but out of reasonable range
-        elif demand < demand_range_table.get(ba, {}).get('min', 0) or demand > demand_range_table.get(ba, {}).get('max', float('inf')):
-            # Rule 1: Use demand forecast value if valid and not in the exception list
-            forecast_value = forecast_table.get(ba, {}).get(hour)
-            if forecast_value is not None and ba not in exception_list:
-                df.at[index, 'Demand'] = forecast_value
+        elif demand < valid_demand_range[ba][0] or demand > valid_demand_range[ba][1]:
+            # Rule for imputing demand values outside the reasonable range
+            imputed_demand = None
+
+            # Rule 1: Use demand forecast value if available and not in the exception list
+            if ba not in forecast_exception_list and pd.notnull(forecast_demand):
+                imputed_demand = forecast_demand
+
+            # Rule 2: Use prior hour's demand value if available
+            elif hour > 0:
+                prior_hour_demand = df.at[index - 1, 'demand']
+                if pd.notnull(prior_hour_demand):
+                    imputed_demand = prior_hour_demand
+
+            # Rule 3: Use prior day's demand value for the corresponding hour if available
+            elif date > df['date'].min():
+                prior_day_demand = df[(df['BA'] == ba) & (df['date'] == date - pd.DateOffset(days=1)) & (df['hour'] == hour)]['demand'].values
+                if len(prior_day_demand) > 0 and pd.notnull(prior_day_demand[0]):
+                    imputed_demand = prior_day_demand[0]
+
+            if imputed_demand is not None:
+                df.at[index, 'demand'] = imputed_demand
             else:
-                # Rule 2: Use prior hour's demand value if valid
-                prior_hour_demand = df[(df['BA'] == ba) & (df['Hour'] == hour - 1)]['Demand'].values
-                if len(prior_hour_demand) > 0 and not pd.isna(prior_hour_demand[0]):
-                    df.at[index, 'Demand'] = prior_hour_demand[0]
-                else:
-                    # Rule 3: Use prior day's demand value for the corresponding hour if valid
-                    prior_day_demand = df[(df['BA'] == ba) & (df['Hour'] == hour - 24)]['Demand'].values
-                    if len(prior_day_demand) > 0 and not pd.isna(prior_day_demand[0]):
-                        df.at[index, 'Demand'] = prior_day_demand[0]
-                    else:
-                        # Rule 3 failed, impute a value of 0
-                        df.at[index, 'Demand'] = 0
+                df.at[index, 'demand'] = 0  # Rule 3 failed, impute a value of 0
 
-    return df
-
-# Replace the column names and exception list with your actual data
 # Example usage:
-# df = impute_demand(df, forecast_table, demand_range_table)
+# impute_demand(df)
