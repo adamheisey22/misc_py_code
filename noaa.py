637f34ba18cff9c127b60168a76a7ef0154e6049
adamheisey22 <admrussel@gmail.com>
2023-12-28 16:11:46 -0700

    final?

--- noaa.py
+++ noaa.py
@@ -1,58 +1,50 @@
+import os
 import requests
+import tarfile
 import pandas as pd
-import sqlite3
-from io import StringIO
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+import dask.dataframe as dd
+from io import BytesIO
 
-def download_weather_data(station_id, year):
-    base_url = "https://www.ncei.noaa.gov/data/global-hourly/access/"
-    url = f"{base_url}{year}/{station_id}-{year}.csv"
+# Function to download and extract data for a specific year
+def download_and_extract(year):
+    url = f'https://www.ncei.noaa.gov/data/global-hourly/archive/csv/{year}.tar.gz'
+    response = requests.get(url)
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return pd.read_csv(StringIO(response.text))
-    except requests.exceptions.RequestException as e:
-        print(f"Failed to download data for station {station_id} and year {year}: {e}")
-        return None
+    if response.status_code == 200:
+        # Extract data from the tar.gz file
+        with tarfile.open(fileobj=BytesIO(response.content), mode="r:gz") as tar:
+            tar.extractall(path=f"./{year}")
+    else:
+        print(f"Failed to download data for {year}")
 
-def download_and_process(station_id, year):
-    weather_data = download_weather_data(station_id, year)
-    if weather_data is not None:
-        weather_data["station_id"] = station_id
-    return weather_data, station_id
+# Download and extract data for 2019 and 2020 concurrently
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    years = [2019, 2020]
+    futures = {executor.submit(download_and_extract, year): year for year in years}
 
-def concatenate_and_write_to_sqlite(stations, year, sqlite_file):
-    conn = sqlite3.connect(sqlite_file)
-    failed_stations = []
+    for future in concurrent.futures.as_completed(futures):
+        year = futures[future]
+        try:
+            future.result()
+            print(f"Download and extraction for {year} completed.")
+        except Exception as e:
+            print(f"Error downloading or extracting data for {year}: {e}")
 
-    # Download and process data in parallel
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(lambda s: download_and_process(s, year), stations))
+# Create Dask DataFrames for each year and concatenate them
+df_list = []
+for year in [2019, 2020]:
+    folder_path = f"./{year}"
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        ddf = dd.read_csv(file_path, assume_missing=True)
+        df_list.append(ddf)
 
-    # Filter out failed downloads
-    successful_results = [result[0] for result in results if result[0] is not None]
-    failed_stations = [result[1] for result in results if result[0] is None]
+# Concatenate all Dask DataFrames into a single Dask DataFrame
+final_ddf = dd.concat(df_list, axis=0)
 
-    if successful_results:
-        # Assuming that the table name is 'weather_data' and the structure is consistent
-        # Adjust the table name and structure based on the actual data structure
-        concatenated_data = pd.concat(successful_results, ignore_index=True)
-        concatenated_data.to_sql("weather_data", conn, if_exists="append", index=False)
+# Compute the result to obtain a Pandas DataFrame
+final_df = final_ddf.compute()
 
-    conn.close()
-
-    return failed_stations
-
-# Example usage
-stations = [123, 456, 789]  # Replace with your actual station IDs
-year = 2019
-sqlite_file = "weather_data.db"
-
-failed_stations = concatenate_and_write_to_sqlite(stations, year, sqlite_file)
-
-# Display the list of failed stations
-if failed_stations:
-    print("Stations failed to download:", failed_stations)
-else:
-    print("All stations downloaded successfully.")
+# Display information about the final DataFrame
+print(final_df.info())
